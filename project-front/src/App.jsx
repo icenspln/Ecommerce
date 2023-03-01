d5f245a1d04c99db19a5cf32ce83131ef3f14fc6
Icen <105867625+icenspln@users.noreply.github.com>
2023-03-01 12:42:55 +0100

    added the details page

--- project-front/src/App.jsx
+++ project-front/src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import Contact from "./containers/Contact";
 import Products from "./containers/Products";
 import Navbar from "./containers/Navbar";
 
+import Details from "./containers/Details";
+
 function App() {
+  const [count, setCount] = useState(1);
+  const handleAddQuantity = () => {
+    if (count <= 5) setCount(count + 1);
+  };
+  const handleRemoveQuantity = () => {
+    if (count > 1) setCount(count - 1);
+  };
+
   return (
     <>
       <main className="container mx-auto px-4 bg-whitish">
         <Navbar />
-        <Products />
+        {/* <Products /> */}
+        <Details
+          count={count}
+          handleAddQuantity={handleAddQuantity}
+          handleRemoveQuantity={handleRemoveQuantity}
+        />
         <Contact />
       </main>
     </>
---
+++ project-front/src/containers/Details.jsx
@@ -0,0 +1,101 @@
+import product2 from "../assets/product2.png";
+import { AiOutlineCheck } from "react-icons/ai";
+const Details = ({ count, handleRemoveQuantity, handleAddQuantity }) => {
+  return (
+    <main>
+      <div className="flex items-center flex-col md:flex-row">
+        <div>
+          <img src={product2} alt="" />
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold my-3">ESSENTIAL AMIN.O. ENERGY</h1>
+          <p className="text-base font-bold my-3">from $30</p>
+          <ul className="leading-9">
+            <li>
+              <span>
+                <AiOutlineCheck className="inline mr-3" />
+              </span>
+              Anytime Energy & Muscle Recovery
+            </li>
+            <li>
+              <span>
+                <AiOutlineCheck className="inline mr-3" />
+              </span>
+              100 mg of Caffeine per Serving from Coffee Bean and/or Tea Leaf
+            </li>
+            <li>
+              <span>
+                <AiOutlineCheck className="inline mr-3" />
+              </span>
+              Anytime Energy & Muscle Recovery5 Grams of Amino Acids for Muscle
+              Recovery Support¹
+            </li>
+            <li>
+              <span>
+                <AiOutlineCheck className="inline mr-3" />
+              </span>
+              5 to 10 Calories Per Serving with Zero Sugar
+            </li>
+          </ul>
+          <hr className="my-6" />
+          <form className="my-4" action="#" method="get">
+            <label className="mr-6" htmlFor="flavor">
+              Flavor
+            </label>
+            <select className="px-3 py-1 bg-white text-redish " name="flavor">
+              <option value="orange">Orange</option>
+              <option value="orange">Apple</option>
+            </select>
+            <br className="mb-6" />
+            <label htmlFor="">Quantity</label>
+            <div className="flex gap-2 items-center ">
+              <div
+                className="bg-white text-redish p-2 cursor-pointer"
+                onClick={handleAddQuantity}
+              >
+                +
+              </div>
+              <div className="bg-white text-redish p-2">{count}</div>
+              <div
+                className="bg-white text-redish p-2 cursor-pointer"
+                onClick={handleRemoveQuantity}
+              >
+                -
+              </div>
+              <p className="font-bold p-3">{count * 30}$</p>
+            </div>
+            <br />
+            <label className="mr-6" htmlFor="size">
+              Size
+            </label>
+            <select className=" px-3 py-1 bg-white text-redish " name="">
+              <option value="29 servings">29 servings</option>
+              <option value="30 servings">30 servings</option>
+              <option value="70 servings">70 servings</option>
+            </select>
+            <button className="block md:inline bg-redish text-white px-3 py-2 mx-auto md:mx-6 my-6 ">
+              ADD TO CART
+            </button>
+          </form>
+          <a className="text-base text-gray-500" href="#">
+            Continue Shopping?
+          </a>
+        </div>
+      </div>
+      <div>
+        <h3 className="text-2xl font-bold">PRODUCT OVERVIEW</h3>
+        <p className="font-thin text-gray-500">
+          Anytime Energy & Muscle Recovery
+        </p>
+        <p className="my-10  ">
+          Mix up ESSENTIAL AMIN.O. ENERGY anytime you want a boost of energy,
+          focus and muscle recovery support. Each serving provides 100 mg of
+          caffeine from natural sources to support energy and focus along with 5
+          grams of amino acids for muscle support.¹
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Details;
